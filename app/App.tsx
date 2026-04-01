d2da4b06700b624f929e64be999d164c4c143ae6
108M <117757556+108M@users.noreply.github.com>
2026-04-01 23:16:50 +0200

    Revert expo-screen-capture due to Expo Go limitation

--- app/App.tsx
+++ app/App.tsx
@@ -3,8 +3,6 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import * as ScreenCapture from 'expo-screen-capture';
-import { useEffect } from 'react';
 // Screens
 import HomeScreen from './src/screens/HomeScreen';
 import LobbyScreen from './src/screens/LobbyScreen';
@@ -17,14 +15,6 @@ import LeaderboardScreen from './src/screens/LeaderboardScreen';
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-  useEffect(() => {
-    // Evitar capturas de pantalla globalmente
-    ScreenCapture.preventScreenCaptureAsync();
-    return () => {
-      ScreenCapture.allowScreenCaptureAsync();
-    };
-  }, []);
-
   return (
     // <-- ENVUELVE TODO EN SafeAreaProvider
     <SafeAreaProvider>
